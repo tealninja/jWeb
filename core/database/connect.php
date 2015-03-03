20aab028fd889cbff052ed48177b5443256c72ba
John Teal <teal.john@gmail.com>
2015-03-02 19:13:41 -0500

    update to openshift standards

--- core/database/connect.php
+++ core/database/connect.php
@@ -4,6 +4,13 @@
 $connect_error = '<h1>No database connection right now</h1>'; //send a custom error message
 $table_error = '<h1>No table found!</h1>';
 
-mysql_connect("vweb","admin5FKc8nS","5e2UT-hVCdbp") or die($connect_error);
-mysql_select_db("vweb") or die('no table');
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
+
+$dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
+$dbh = new PDO($dsn, DB_USER, DB_PASS);
+
 ?>
