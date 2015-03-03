b1d6f88209652b9ba83348ab9b1a37b099eec8e6
John Teal <teal.john@gmail.com>
2015-03-02 19:53:25 -0500

    checking on update to PDO vs mysql

--- core/database/connect.php
+++ core/database/connect.php
@@ -2,15 +2,19 @@
 
 //GIT UPLOAD
 $connect_error = '<h1>No database connection right now</h1>'; //send a custom error message
-$table_error = '<h1>No table found!</h1>';
-
 define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
 define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
 
-$dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
-$dbh = new PDO($dsn, DB_USER, DB_PASS);
+try{
+
+    $dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
+    $db = new PDO($dsn, DB_USER, DB_PASS));
+} catch(PDOException $e){
+    echo 'ERROR:' . $e->getMessage()
+}
+//from http://stackoverflow.com/questions/15921169/how-to-connect-to-the-database-in-openshift-application
 
 ?>
