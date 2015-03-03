1ae858c8448edc7eecfdb6995d60d75027fcf979
John Teal <teal.john@gmail.com>
2015-03-02 19:03:19 -0500

    db update

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,8 +1,9 @@
 <?php
 
 //GIT UPLOAD
-$connect_error = 'No database connection right now'; //send a custom error message
+$connect_error = '<h1>No database connection right now</h1>'; //send a custom error message
+$table_error = '<h1>No table found!</h1>';
 
 mysql_connect("vweb","admin5FKc8nS","5e2UT-hVCdbp") or die($connect_error);
-mysql_select_db("lr") or die('no table');
+mysql_select_db("vweb") or die('no table');
 ?>
