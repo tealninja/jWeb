566bb0e3c138e04f9eb15dd5a596776d6736cba6
John Teal <teal.john@gmail.com>
2015-03-02 19:56:14 -0500

    update connect

--- core/database/connect.php
+++ core/database/connect.php
@@ -8,13 +8,9 @@ define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
 
-try{
+$dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
+$dbh = new PDO($dsn, DB_USER, DB_PASS));
 
-    $dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
-    $db = new PDO($dsn, DB_USER, DB_PASS));
-} catch(PDOException $e){
-    echo 'ERROR:' . $e->getMessage()
-}
 //from http://stackoverflow.com/questions/15921169/how-to-connect-to-the-database-in-openshift-application
 
 ?>
