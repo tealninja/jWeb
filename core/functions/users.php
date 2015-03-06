b2e567bcfcecf1709282bd42b7107607d8b11004
John Teal <teal.john@gmail.com>
2015-03-05 20:37:50 -0500

    hmmm

--- core/functions/users.php
+++ core/functions/users.php
@@ -94,8 +94,12 @@ function change_password($user_id, $password) {
 }
 
 function register_user($register_data){
-    $register_data['password'] =md5($register_data['password']);
+    //array_walk($register_data, 'array_sanitize');
+    //$register_data['password'] =md5($register_data['password']);
     
+    //$fields = '`' . implode('`, `',array_keys($register_data)) . '`';
+    //$data = '\'' . implode('\', \'', $register_data) . '\'';
+
     global $db;
     //prepare query
     $query = $db->prepare("INSERT INTO user (`username`, `password`, `first_name`, `last_name`, `email`, `email_code`) VALUES (:username, :password, :first_name, :last_name, :email, :email_code)");
