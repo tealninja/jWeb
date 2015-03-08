3dc9866377f519e8254141b2b616b843f3273a77
John Teal <teal.john@gmail.com>
2015-03-08 19:09:42 -0400

    added /messages to domain

--- core/functions/general.php
+++ core/functions/general.php
@@ -7,7 +7,7 @@ function email($to, $subject, $body) {
     //mail($to, $subject, $body, 'From: Hello@mywebsite.org');
     //mail('teal.john@gmail.com', 'new user!', 'someone signed up', 'From: teal.john@gmail.com');
     $api_key = 'key-9c6b17aa0ea761f75753b6d202616265';//key from mailgun
-    $domain = 'https://api.mailgun.net/v2/' . 'sandbox1190eeea19d443a19cbeb3e1163268ab.mailgun.org';
+    $domain = 'https://api.mailgun.net/v2/' . 'sandbox1190eeea19d443a19cbeb3e1163268ab.mailgun.org' . '/messages';
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
     curl_setopt($ch, CURLOPT_USERPWD, 'api:' . $api_key);
