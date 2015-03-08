b9697d7adbf01cf8c9862d59289aa542860c5c85
John Teal <teal.john@gmail.com>
2015-03-08 18:34:54 -0400

    added mailgun to email function

--- core/functions/general.php
+++ core/functions/general.php
@@ -3,9 +3,24 @@
 
 //sends an email
 function email($to, $subject, $body) {
+    //sends email using mailgun; see https://blog.openshift.com/email-in-the-cloud-with-mailgun/
     //mail($to, $subject, $body, 'From: Hello@mywebsite.org');
-    mail('teal.john@gmail.com', 'new user!', 'someone signed up', 'From: teal.john@gmail.com');
+    //mail('teal.john@gmail.com', 'new user!', 'someone signed up', 'From: teal.john@gmail.com');
 
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($ch, CURLOPT_USERPWD, 'api:key-9c6b17aa0ea761f75753b6d202616265');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_URL, 'sandbox1190eeea19d443a19cbeb3e1163268ab.mailgun.org'); //update with my domain
+    curl_setopt($ch, CURLOPT_POSTFIELDS,
+        array(  'from'      =>'John Teal <teal.john@gmail.com>',
+                'to'        =>$to,
+                'subject'   => $subject,
+                'text'      => $body));
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
 }
 
 function logged_in_redirect(){
