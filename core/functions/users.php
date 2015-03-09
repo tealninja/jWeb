16b1fd44f24f87b4a2f1d9a97f7d4a25adf1f55e
John Teal <teal.john@gmail.com>
2015-03-08 21:22:27 -0400

    updating query on email_exists

--- core/functions/users.php
+++ core/functions/users.php
@@ -175,6 +175,8 @@ function email_exists($email){
     $stmt->bindValue(":email", $email, PDO::PARAM_STR);
     $stmt->execute();
     $result = $stmt->fetch();
+    echo $result;
+    die();
     return ($result >=1) ? true:false;
     //$query = mysql_query("SELECT COUNT(`user_id`) FROM `user` WHERE `email` = '$email'");
     //return (mysql_result($query, 0) == 1) ? true : false;
